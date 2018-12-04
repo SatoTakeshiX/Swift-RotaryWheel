256a135e5403a4faa3ae55174f63e57efbcec91a
satoutakeshi <sato.takeshi1987@gmail.com>
2018-12-04 16:03:36 +0900

    Initial Commit

---
+++ RotaryWheel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RotaryWheel
+//
+//  Created by satoutakeshi on 2018/12/04.
+//  Copyright © 2018年 Personal Factory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
